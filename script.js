35914ef131871f69368b6f4f1f1ada91a233a337
javierpalmaromero09-star <javierpalmaromero09@gmail.com>
2026-08-04 20:35:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ cuenta: document.getElementById("cuenta").value,
 correo: document.getElementById("correo").value,
 folio: "RMA-" + Math.floor(Math.random()*90000+10000)
 };
+
+document.getElementById("registroForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+
+    let datos = {
+
+        nombre: document.getElementById("nombre").value,
+        cuenta: document.getElementById("cuenta").value,
+        escuela: document.getElementById("escuela").value,
+        fecha: document.getElementById("fecha").value,
+        correo: document.getElementById("correo").value,
+        promedio: document.getElementById("promedio").value,
+        materia: document.getElementById("materia").value,
+        semestre: document.getElementById("semestre").value,
+
+        folio: "RMA-" + Math.floor(Math.random()*90000+10000)
+
+    };
+
+
+    localStorage.setItem(
+        "registroMentores",
+        JSON.stringify(datos)
+    );
+
+
+    window.location.href="comprobante.html";
+
+
+});
