668dfdf0a0585281405b5ed7c7cd325ccbfb9a82
javierpalmaromero09-star <javierpalmaromero09@gmail.com>
2026-08-04 20:16:22 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let datos = {
+nombre: document.getElementById("nombre").value,
+escuela: document.getElementById("escuela").value,
+cuenta: document.getElementById("cuenta").value,
+correo: document.getElementById("correo").value,
+folio: "RMA-" + Math.floor(Math.random()*90000+10000)
+};
